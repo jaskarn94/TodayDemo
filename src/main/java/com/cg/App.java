0045d9d06a53e56a5e50aa9b7076f72b03465281
jaskarn94 <jaskarn94177@gmail.com>
2017-11-13 13:32:30 +0530

    Message shown

---
+++ src/main/java/com/cg/App.java
@@ -0,0 +1,14 @@
+package com.cg;
+
+import java.io.EOFException;
+
+public class App {
+	public static void main (String [] args){
+		int i;
+		try {
+			System.out.println("Utoo ni fragnance laake nikle");
+		}
+		 catch (Exception e){}
+	}
+
+}
